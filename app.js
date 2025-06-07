c23708fef8d58c00eff7e19e3b8eeb1d9ab09df3
Tautvydas <tmurinast@gmail.com>
2025-06-08 02:17:06 +0300

    Move node modules to server folder

--- app.js
+++
@@ -1,9 +0,0 @@
-const socket = io('ws://localhost:8080');
-
-socket.onmessage = ({ data }) => {
-    console.log('Message from server ', data);
-}
-
-docket.querySelector('button').onclick = () => {
-    socket.send('Hello Server!');
-}
