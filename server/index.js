5185f4c008ebba79c2c08588772a2b8b97d00233
Tautvydas <tmurinast@gmail.com>
2025-06-08 02:52:18 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const { SerialPort } = require('serialport');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    const filePath = path.join(__dirname, 'client.html');
+    const filePath = path.join(__dirname, 'index.html');
     fs.readFile(filePath, (err, data) => {
       if (err) {
         res.writeHead(500);
-        res.end('Error loading client.html');
+        res.end('Error loading index.html');
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
