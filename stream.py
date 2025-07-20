609d1b66e93689bf7d23074434e37a2986ad7219
Tautvydas <tmurinast@gmail.com>
2025-07-20 23:35:26 +0300

    m

--- stream.py
+++ stream.py
@@ -1,7 +1,7 @@
+from aiohttp import web
 import numpy as np
 import cv2
 from aiortc import VideoStreamTrack, RTCPeerConnection, RTCSessionDescription
-from aiohttp import web
 from av import VideoFrame
 
 cap = cv2.VideoCapture(0)
@@ -12,11 +12,9 @@ class PiCameraStream(VideoStreamTrack):
 
     async def recv(self):
         pts, time_base = await self.next_timestamp()
-
         ret, frame = cap.read()
         if not ret:
             frame = np.zeros((720, 1280, 3), dtype=np.uint8)
-
         video_frame = VideoFrame.from_ndarray(frame, format="bgr24")
         video_frame.pts = pts
         video_frame.time_base = time_base
@@ -24,12 +22,13 @@ class PiCameraStream(VideoStreamTrack):
 
 async def offer(request):
     if request.method == "OPTIONS":
-        # Handle preflight CORS request
-        resp = web.Response()
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return resp
+        # Reply to preflight request
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+        }
+        return web.Response(status=204, headers=headers)
 
     params = await request.json()
     offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
@@ -42,14 +41,15 @@ async def offer(request):
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
-    resp = web.json_response({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return resp
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+    }
+    return web.json_response({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type}, headers=headers)
 
 app = web.Application()
-app.router.add_route('*', '/offer', offer)
+app.router.add_route("*", "/offer", offer)
 
 if __name__ == "__main__":
-    web.run_app(app, host='0.0.0.0', port=8000)
+    web.run_app(app, host="0.0.0.0", port=8000)
