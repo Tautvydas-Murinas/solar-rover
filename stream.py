63816a588f12396b07db81b7c8ade764e4e3dd15
Tautvydas <tmurinast@gmail.com>
2025-07-20 22:40:25 +0300

    Update

--- stream.py
+++ stream.py
@@ -1,34 +1,46 @@
-from aiortc import VideoStreamTrack, RTCPeerConnection
+import numpy as np
+import cv2
+from aiortc import VideoStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiohttp import web
 from av import VideoFrame
 
-def get_camera_frame():
-    return VideoFrame.from_ndarray(np.zeros((720, 1280, 3), dtype=np.uint8), format='bgr24')
+# OpenCV video capture from default camera (e.g., /dev/video0)
+cap = cv2.VideoCapture(0)
 
 class PiCameraStream(VideoStreamTrack):
     def __init__(self):
         super().__init__()
-        self._frame = None
 
     async def recv(self):
-        frame = get_camera_frame()
-        return frame
+        pts, time_base = await self.next_timestamp()
+
+        ret, frame = cap.read()
+        if not ret:
+            # Return a black frame if camera fails
+            frame = np.zeros((720, 1280, 3), dtype=np.uint8)
+
+        # Convert OpenCV BGR frame to VideoFrame
+        video_frame = VideoFrame.from_ndarray(frame, format="bgr24")
+        video_frame.pts = pts
+        video_frame.time_base = time_base
+        return video_frame
 
 async def offer(request):
     params = await request.json()
-    offer_sdp = params['sdp']
+    offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
+
     pc = RTCPeerConnection()
+    local_video = PiCameraStream()
+    pc.addTrack(local_video)
 
-    local_video_track = PiCameraStream()
-    pc.addTrack(local_video_track)
+    await pc.setRemoteDescription(offer)
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
 
-    await pc.setRemoteDescription(offer_sdp)
-
-    answer_sdp = await pc.createAnswer()
-    await pc.setLocalDescription(answer_sdp)
-
-    return web.json_response({'sdp': pc.localDescription.sdp})
+    return web.json_response({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
 
 app = web.Application()
 app.router.add_post("/offer", offer)
-web.run_app(app, port=8000)
+
+if __name__ == "__main__":
+    web.run_app(app, port=8000)
