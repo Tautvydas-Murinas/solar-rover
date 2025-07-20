3c4fc4ef3dc92e952bf120a18594a629e0929015
Tautvydas <tmurinast@gmail.com>
2025-07-20 23:34:13 +0300

    m

--- stream.py
+++ stream.py
@@ -4,7 +4,6 @@ from aiortc import VideoStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiohttp import web
 from av import VideoFrame
 
-# OpenCV video capture from default camera (e.g., /dev/video0)
 cap = cv2.VideoCapture(0)
 
 class PiCameraStream(VideoStreamTrack):
@@ -16,16 +15,22 @@ class PiCameraStream(VideoStreamTrack):
 
         ret, frame = cap.read()
         if not ret:
-            # Return a black frame if camera fails
             frame = np.zeros((720, 1280, 3), dtype=np.uint8)
 
-        # Convert OpenCV BGR frame to VideoFrame
         video_frame = VideoFrame.from_ndarray(frame, format="bgr24")
         video_frame.pts = pts
         video_frame.time_base = time_base
         return video_frame
 
 async def offer(request):
+    if request.method == "OPTIONS":
+        # Handle preflight CORS request
+        resp = web.Response()
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return resp
+
     params = await request.json()
     offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
 
@@ -37,10 +42,14 @@ async def offer(request):
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
-    return web.json_response({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
+    resp = web.json_response({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return resp
 
 app = web.Application()
-app.router.add_post("/offer", offer)
+app.router.add_route('*', '/offer', offer)
 
 if __name__ == "__main__":
-    web.run_app(app, port=8000)
+    web.run_app(app, host='0.0.0.0', port=8000)
