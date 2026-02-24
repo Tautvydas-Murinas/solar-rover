9b04af3816223afdf763c805d78cb832e23c4bdf
TautvydasMurinas <tmurinast@gmail.com>
2026-02-24 17:26:41 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,35 +1,14 @@
-from flask import Flask, Response
-from picamera2 import Picamera2
 import cv2
 
-app = Flask(__name__)
+cap = cv2.VideoCapture(0)  # 0 = default camera
 
-picam2 = Picamera2()
-config = picam2.create_video_configuration(main={"size": (640, 480)})
-picam2.configure(config)
-picam2.start()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow("Camera", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-
-def gen_frames():
-    while True:
-        frame = picam2.capture_array()
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/')
-def index():
-    return "<img src='/video'>"
-
-
-@app.route('/video')
-def video():
-    return Response(gen_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run(host='192.168.1.229', port=8000, threaded=True)
+cap.release()
+cv2.destroyAllWindows()
